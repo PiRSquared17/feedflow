1a15a4191af6a5894a79d143f96c69f5a5956fb9
fasdfasdas <fasdfasdas@localhost>
2010-12-10 17:56:33 +0200

    auto update system implemented

--- scripts/au.js
+++ scripts/au.js
@@ -1,5 +1,87 @@
-﻿function getNewss()
-{
-	mainContainer.innerHTML=fileList.join("yo biç");
+﻿var fileList = Array();
+var fileContents = Array();
 
+function createFileList(i)
+{
+	var bList = Array();
+	if(i==undefined)i="";
+	var listHTTP=new XMLHttpRequest();
+	listHTTP.onreadystatechange=function () {
+		if(listHTTP.readyState==4){
+			if(listHTTP.status==200){
+				bList = listHTTP.responseText.match(/\<li\><\a.+?\>(.+)\<\/a\>\<\/li\>/ig).join().replace(/\<.+?\>/g,"").split(",");
+				//fileList=fileList.concat(bList);
+				for(x in bList)
+					if(bList[x]!=".." && !bList[x].match(/\/$/) && bList[x]!="Gadget.html" && bList[x]!="au.js" && bList[x]!="gadget.js")
+						fileList.push(i+bList[x]);
+			}
+		}
+	}
+	listHTTP.open("GET","http://feedflow.googlecode.com/hg/"+i,false);
+	listHTTP.send(null);
+	for(n in bList)
+		if(bList[n].match(/\/$/))
+			createFileList(i+bList[n]);
 }
+
+function downloadFileContents(){
+	for(x in fileList){
+		var uHTTP=new XMLHttpRequest();
+		uHTTP.onreadystatechange=function () {
+			if(uHTTP.readyState==4){
+				if(uHTTP.status==200){
+					fileContents[x]=uHTTP.responseBody;
+				}
+			}
+		}
+		uHTTP.open("GET","http://feedflow.googlecode.com/hg/"+fileList[x]+"?"+Math.random(),false);
+		uHTTP.send(null);
+	}
+}
+
+function performBgUpdate()
+{
+	createFileList();
+	downloadFileContents();
+
+	for(x in fileList)
+		binWriteFile(fileContents[x],System.Gadget.path+"\\"+fileList[x].replace(/\//g,"\\"));
+}
+
+function binWriteFile(binData, filePath)
+{
+    var fstream = new ActiveXObject("ADODB.Stream");
+    fstream.Type = 1;
+    fstream.Open;
+    fstream.Write(binData);
+    fstream.SaveToFile(filePath, 2);
+    fstream.Close;
+}
+
+function binReadFile(filePath)
+{
+    var fstream = new ActiveXObject("ADODB.Stream");
+    fstream.Type = 1;
+    fstream.Open;
+    fstream.LoadFromFile(filePath);
+    return fstream.Read;
+}
+
+/*fileList.sort(function(x,y){ 
+      var a = String(x).toUpperCase(); 
+      var b = String(y).toUpperCase(); 
+      if (a > b) 
+         return 1 
+      if (a < b) 
+         return -1 
+      return 0; 
+    });
+	
+if(fileList[x].match(/\.css$/)) // BU EN SON YAPILACAK, AU.JS DEĞİŞTİRMESİ YARIDA KESİYOR-css güncellenmiyor
+		eval(fileList[x].replace(/[\/\.]/g,"")+".href='"+fileList[x]+"'"); // css güncellenmiyor, zaten gereksiz
+	*/
+	
+/*for(x in fileList){
+		if(fileList[x].match(/\.js$/)) //html code changes does not apply until restart so this is unnecessary and may cause bugs
+		eval(fileList[x].replace(/[\/\.]/g,"")+".src='"+fileList[x]+"'");
+	}*/
