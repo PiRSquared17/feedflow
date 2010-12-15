b912d939532fc36a841e1ca52a75801e01cdcff6
fasdfasdas <fasdfasdas@localhost>
2010-12-15 13:03:47 +0200

    update date information removed from files

--- scripts/au.js
+++ scripts/au.js
@@ -1,4 +1,29 @@
-﻿var fileList = Array();
+﻿/**********************************************
+
+ *	Filename:	au.js
+ *	Authors:	Tolga Hosgor, Cristian Patrasciuc
+ *	Emails:		fasdfasdas@gmail.com, cristian.patrasciuc@gmail.com
+ 
+ Copyright © 2010 Tolga Hosgor, Cristian Patrasciuc
+ 
+ This file is part of FeedFlow.
+
+ FeedFlow is free project: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ FeedFlow is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with FeedFlow.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **********************************************/
+
+var fileList = Array();
 var fileContents = Array();
 
 function createFileList(i)
